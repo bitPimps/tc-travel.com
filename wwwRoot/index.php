bd7c38fecf7c0ca97180324af0cc7893cff45202
bitPimps <bitpimps@gmail.com>
2020-02-03 14:42:15 -0600

    Update index.php
    Upgrading jQuery:
    1.11.2 > 11.12.4
    
    Upgrading Bootstrap:
    3.3.7 > 3.4.1

--- wwwRoot/index.php
+++ wwwRoot/index.php
@@ -9,8 +9,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>A Travel Concierge</title>
 		<meta name="description" content="">
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" />
 		<link href="/css/main.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
@@ -45,8 +45,8 @@
 			<p class="text-muted"><small>&copy; <?php echo date("Y"); ?> The Upledger Institute International, Inc.<sup>&reg;</sup></small></p>
 		</footer>
 	</div><!-- /container -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-10031363-4"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
