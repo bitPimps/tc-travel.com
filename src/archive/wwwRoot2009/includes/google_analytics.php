72495b70dcd97593633aaccfad783b1d955c2e30
Scott Lix <scott.lix@lixlink.com>
2025-12-02 14:15:52 -0600

    Github page

--- src/archive/wwwRoot2009/includes/google_analytics.php
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-10031363-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-10031363-2');
-</script>
